6121f22b4baabda377fd1036df18af848672e240
Sonny Li <sonnynomnom@gmail.com>
2021-12-01 14:29:37 -0500

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -1,11 +1,7 @@
-# Advent of Code 2021 - Day 1
 # https://adventofcode.com/2021/day/1
-
-# Move each number from depths.txt into a list called depth
 
 with open('/Users/sonny/Desktop/advent-of-code-2021/day1/depths.txt', 'r') as file:
   depths = [int(line.strip()) for line in file]
-  # print(depth)
 
 # ========== Part 1 ==========
 
@@ -15,7 +11,7 @@ for i in range(len(depths)):
   if (depths[i] - depths[i-1]) > 0:
     increased += 1
 
-print(increased) # Output: 1557
+print(increased)
 
 # ========== Part 2 ==========
 
@@ -26,12 +22,10 @@ for i in range(len(depths)):
   sum = depths[i] + depths[i-1] + depths[i-2]
   sliding_window.append(sum)
 
-# print(sliding_window)
-
 increased = 0
 
 for i in range(len(sliding_window)):
   if (sliding_window[i] - sliding_window[i-1]) > 0:
     increased += 1
 
-print(increased) # Output: 1608
+print(increased)
